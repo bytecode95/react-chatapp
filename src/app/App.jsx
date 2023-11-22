b719ff3268b0066cbb000db77dfbc022f08647c4
chethanavirajini <cheth.virajini@gmail.com>
2023-11-22 11:47:28 +0530

    chat Feed

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import {ChatEngine} from 'react-chat-engine';
 import ChatFeed from '../components/ChatFeed';
+import './App.css'
 
 function App() {
 
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,9 +1,21 @@
 
 
 export default function MyMessage(){
+    if(message?. attachment?.length>0){
+        return(
+            <img 
+                src={message.attachment[0].file}
+                alt="message-attachement"
+                className="message-image"
+                style={{float:'right'}}
+            />
+        )
+    }
     return (
         <>
-            MyMessage
+           <div className="message" style={{float:'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}}>
+                {message.text}
+            </div>
         </>
     )
 }
